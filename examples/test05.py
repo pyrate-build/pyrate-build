70119522937fdc9ebfdf9473bbedbce4485759c4
Fred Stober <stober@cern.ch>
2016-02-08 11:14:03 +0100

    updated test to use lib that exists on the travis ci docker image

--- examples/test05.py
+++ examples/test05.py
@@ -4,13 +4,19 @@ import logging, os
 logging.critical(repr(list(filter(lambda x: x.endswith('.a'), os.listdir('/usr/lib')))))
 logging.critical(repr(list(filter(lambda x: x.endswith('.so'), os.listdir('/usr/lib')))))
 
-executable('example12a.bin', ['test.cpp', 'foo.cpp', static_library('/usr/lib/libdl')])
-executable('example12b.bin', ['test.cpp', 'foo.cpp', shared_library('/usr/lib/libdl')])
+try:
+	executable('example12a.bin', ['test.cpp', 'foo.cpp', static_library('/usr/lib/libpython2.7.a')])
+except Exception:
+	logging.critical('referencing static library')
+try:
+	executable('example12b.bin', ['test.cpp', 'foo.cpp', shared_library('/usr/lib/libpython2.7.so')])
+except Exception:
+	logging.critical('referencing shared library')
 try:
 	executable('example12c.bin', ['test.cpp', 'foo.cpp', static_library('/usr/lib/libfoo')])
 except Exception:
-	logging.critical('referencing library')
+	logging.critical('referencing bogus static library')
 try:
 	executable('example12d.bin', ['test.cpp', 'foo.cpp', shared_library('/usr/lib/libfoo')])
 except Exception:
-	logging.critical('referencing library')
+	logging.critical('referencing bogus shared library')
