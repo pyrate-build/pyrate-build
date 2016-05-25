a3494504e73ecc7a952d01c43fea70a7743f8503
Fred Stober <stober@cern.ch>
2016-05-25 20:13:16 +0200

    Removed refactored test code

--- examples/test05.py
+++ examples/test05.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env pyrate
 
 import logging, os
-logging.critical(repr(list(filter(lambda x: x.endswith('.a'), os.listdir('/usr/lib')))))
-logging.critical(repr(list(filter(lambda x: x.endswith('.so'), os.listdir('/usr/lib')))))
 
-try:
-	executable('test05a.bin', ['test.cpp', 'foo.cpp', static_library('/usr/lib/libpython2.7.a')])
-except Exception:
-	logging.critical('referencing static library')
-try:
-	executable('test05b.bin', ['test.cpp', 'foo.cpp', shared_library('/usr/lib/libpython2.7.so')])
-except Exception:
-	logging.critical('referencing shared library')
-try:
-	executable('test05c.bin', ['test.cpp', 'foo.cpp', static_library('/usr/lib/libfoo')])
-except Exception:
-	logging.critical('referencing bogus static library')
-try:
-	executable('test05d.bin', ['test.cpp', 'foo.cpp', shared_library('/usr/lib/libfoo')])
-except Exception:
-	logging.critical('referencing bogus shared library')
+def display_libs(dn, ext):
+	logging.critical(repr(list(filter(lambda x: x.endswith(ext), os.listdir(dn)))))
+for ext in ['.a', '.so']:
+	display_libs('/usr/lib', ext)
+
+def check_linking(name, libtype, libname, msg):
+	try:
+		executable(name, ['test.cpp', 'foo.cpp', libtype(libname)])
+	except Exception:
+		logging.critical('%s: %s %s', name, libtype, libname)
+		logging.critical(msg)
+check_linking('test05a.bin', static_library, '/usr/lib/libpython2.7.a', 'referencing static library')
+check_linking('test05b.bin', shared_library, '/usr/lib/libpython2.7.so', 'referencing shared library')
+check_linking('test05c.bin', static_library, '/usr/lib/libfoo', 'referencing bogus static library')
+check_linking('test05d.bin', shared_library, '/usr/lib/libfoo', 'referencing bogus shared library')
